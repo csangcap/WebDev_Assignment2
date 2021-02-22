6f9989a96e4b23ccaef94297cdb9a49e35007d61
Caitlin Sangcap <csangcap405@gmail.com>
2021-02-22 16:24:51 -0500

    added the comments for the filter() function

--- assignment2.js
+++ assignment2.js
@@ -49,6 +49,9 @@ arr.myEach(print);
 //======================================================================
 
 /*
+
+map()
+
 Without using the native “Array.prototype.map” method of JavaScript, 
 compose a function titled “myMap” that will take in an array of elements
 and execute a callback function on each of those elements.
@@ -75,5 +78,40 @@ parameters:
 returns: A new array with each element being the result of the callback function.
 */
 
+//======================================================================
 
+/*
+filter()
 
+Without using the native “Array.prototype.filter” method of JavaScript, 
+compose a function titled “myFilter” that will take in an array of elements 
+and execute a callback function on each of those elements.
+
+The filter() method creates a new array with all elements that pass 
+the test implemented by the provided function.
+
+Description
+
+filter() calls a provided callback function once for each element in an 
+array, and constructs a new array of all the values for which callback 
+returns a value that coerces to true. callback is invoked only for indexes 
+of the array which have assigned values; it is not invoked for indexes which 
+have been deleted or which have never been assigned values. Array elements 
+which do not pass the callback test are skipped, and are not included in 
+the new array.
+
+Parameters:
+
+callback
+Function is a predicate, to test each element of the array. Return a 
+value that coerces to true to keep the element, or to false otherwise.
+
+It accepts three arguments:
+  currentValue: The current element being processed in the array.
+  index(Optional): The index of the current element being processed in the array.
+  array(Optional): The array filter was called upon.
+  thisArg(Optional): Value to use as this when executing callback.
+
+Return value: A new array with the elements that pass the test. 
+    If no elements pass the test, an empty array will be returned.
+*/
