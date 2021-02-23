72417c93cf0e9353b138fc95fba5e019c619f3c6
Caitlin Sangcap <csangcap405@gmail.com>
2021-02-23 16:09:57 -0500

    the myMap() function works now

--- assignment2.js
+++ assignment2.js
@@ -2,7 +2,7 @@
 Web Development - Spring 2021
 Assignment 2
 Team members:
-  Caitlin-Dawn Sangcap
+  Caitlin-Dawn Sangcap (created the file, the repo and did myMap() and myFilter() functions)
   Alfonso Gunawan
 */
 
@@ -39,12 +39,12 @@ const cb1 =(x) =>{
 	console.log(isOddNumber(x));
 };
 
-const print1 = (x) => console.log(x);
+const print = (x) => console.log(x);
 
-print1("myEach() test:")
+print("myEach() test:")
 arr1.myEach(cb1);
-print1("--");
-arr1.myEach(print1);
+print("--");
+arr1.myEach(print);
 
 //======================================================================
 
@@ -80,24 +80,26 @@ returns: A new array with each element being the result of the callback function
 
 Array.prototype.myMap = function(callback) 
 {
-	//no idea what to put. please help
-	//I have no clue what to do here. This makes no sense to me.
+	var answer = [];
+	for (let i =0; i<this.length; i++) {
+		//if(this[i]===undefined) {continue;}
+		answer.push(callback(this[i], i , this));
+	}
+	return answer; //returns undefined
 };
 
 //Testing
-const arr2 = [1,2,3,4];
+const arr2 = [5,6,7,8];
 function timesTwo(number){
 	return number * 2;
 }
-const cb2 =(x) => {
-	console.log(timesTwo(x))
-};
 
-print1("myMap() test:");
+print("myMap() test:");
 
-//var arr3=[];
-print1(cb2);
-//print1(arr3);
+var trial2 = arr2.myMap(timesTwo);
+print(trial2);
+print("--");
+print(arr2);
 
 
 //======================================================================
